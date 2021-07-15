cd441f42383293c259b698b246a6c258dd53173e
Otavio Augusto <hwe.otavio@hotmail.com>
2021-07-14 22:34:59 -0300

    update

--- js/app.js
+++ js/app.js
@@ -15,6 +15,12 @@ btnSignup.addEventListener("click", function (){
 
 @keyframes slidein {
     from {
-
+      margin-left: 100%;
+      width: 300%;
     }
-}
+  
+    to {
+      margin-left: 0%;
+      width: 100%;
+    }
+  }
